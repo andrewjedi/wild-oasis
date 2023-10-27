8b179c6bd672c73b78862636f7695d4efe3e9753
Andrew Thompson <andrewthompson@Andrews-M1-16-MBP-Pro.local>
2023-10-27 14:16:55 -0700

    still going

--- src/features/settings/useSettings.js
+++ src/features/settings/useSettings.js
@@ -1,5 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
+import { getSettings } from '../../services/apiSettings';
 
 export function useSettings() {
-  const { isLoading, error, data: settings } = useQuery();
+  const {
+    isLoading,
+    error,
+    data: settings,
+  } = useQuery({
+    queryKey: ['settings'],
+    queryFn: getSettings,
+  });
+
+  return { isLoading, error, settings };
 }
